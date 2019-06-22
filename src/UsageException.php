3ca16976343c33db85676807eb9ef84c54f07643
Mark A. Hershberger <mah@nichework.com>
2019-06-22 17:38:25 -0400

    Add UsageException

---
+++ src/UsageException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace splitbrain\phpcli;
+
+/**
+ * Usage exception for argument warnings and such
+ *
+ * @author Mark A. Hershberger <mah@nichework.com>
+ * @license MIT
+ */
+class UsageException extends Exception {
+}
